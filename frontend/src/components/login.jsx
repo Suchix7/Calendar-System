88f5d9a3e1fdad1885bc35d496896724faca7220
Sujal Chitrakar <sujalchitrakar9851182571@gmail.com>
2026-03-15 22:24:17 +0545

    fixed wrong naming convention

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Lock, Mail, Eye, EyeOff, Church, Loader2 } from "lucide-react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const ChurchLogin = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({ email: "", password: "" });
